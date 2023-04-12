c3238e5c4cd8923fa3e0d3420d3630436c87e24e
Luigi Cardone <lcardone22@gmail.com>
2023-04-12 13:45:32 +0200

    Update searchController.js

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,6 +4,7 @@ import mysql from 'mysql'
 import util from 'util'
 const config = JSON.parse(fs.readFileSync('db_config.json'))
 const fb_user = JSON.parse(fs.readFileSync('fb_config.json'))
+console.log(fb_user.user)
 const search = new Search(5, fb_user.user, fb_user.password)
 
 const db = mysql.createConnection(config);
@@ -17,15 +18,15 @@ var failures = 0
 for(let car of data ){
   const geo_info = await query('SELECT * FROM `italy_munic` WHERE `comune` = ?', [car.geo_town])
   try{
-    await query('INSERT INTO `cars_facebook`(`urn`, `subject`, `price`, `mileage_scalar`, `register_year`, `geo_region`, `geo_provincia`, `geo_town`, `url`) VALUES (?,?,?,?,?,?,?,?,?)',
-    [car.urn, car.subject, car.price, car.mileage_scalar, car.register_year, car.geo_region, geo_info[0].provincia, car.geo_town, car.url])
+    await query('INSERT INTO `cars_facebook`(`urn`, `subject`, `price`, `mileage_scalar`, `register_year`, `geo_region`, `geo_provincia`, `geo_town`, `url`, `advertiser_name`, `advertiser_phone`) VALUES (?,?,?,?,?,?,?,?,?,?,?)',
+    [car.urn, car.subject, car.price, car.mileage_scalar, car.register_year, car.geo_region, geo_info[0].provincia, car.geo_town, car.url, car.advertiser_name, car.advertiser_phone])
     console.log(`Element ${car.subject} added to database`)
   }
   catch(err){
     console.log("Unable to add current item")
     failures++
   }
-
+  
 }
 console.log(`${data.length - failures} out of ${data.length} were added successfully to the database`)
 try{
@@ -35,5 +36,6 @@ try{
 catch(err){
   console.log(err)
 }
+
 
 process.exit()
